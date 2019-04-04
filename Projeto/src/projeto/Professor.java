b0ece9f47732f1b8c4c79e4a206b5ccab159cc5f
05200235 <05200235@D54988.farroupilha.ifrs.edu.br>
2019-04-04 16:56:29 -0300

    Projeto inicial.

---
+++ Projeto/src/projeto/Professor.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+/**
+ *
+ * @author coelho
+ */
+public class Professor {
+
+    private String nome;
+    private String areas[];
+    private long siape;
+
+    public Professor(String nome, String[] areas, long siape) {
+        this.nome = nome;
+        this.areas = areas;
+        this.siape = siape;
+    }
+
+    public String[] getAreas() {
+        return areas;
+    }
+
+    public void setAreas(String[] areas) {
+        this.areas = areas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getSiape() {
+        return siape;
+    }
+
+    public void setSiape(long siape) {
+        this.siape = siape;
+    }
+    
+    void alterarNome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    boolean novaArea(String area) {
+        for (int i = 0; i < areas.length; i++) {
+            if (areas[i] == null) {
+                areas[i] = area;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    boolean removerArea(String area) {
+        for (int i = 0; i < areas.length; i++) {
+            if (areas[i].equals(area)) {
+                areas[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+}
